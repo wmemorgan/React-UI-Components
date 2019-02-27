c0fe7a85ef66e3884287daa8defb1d3cad1cd7be
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-27 18:46:42 +0800

    Create Footer component

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <nav>
+        <i className="far fa-comment"></i>
+        <i className="fas fa-sync"></i>
+        <i className="far fa-heart"></i>
+        <i className="far fa-envelope"></i>
+      </nav>
+    </footer>
+  )
+}
+
+export default Footer;
