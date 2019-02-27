d29af25a63fe89aad2893a191e5abeb4dfbe5dcc
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-27 14:18:41 +0800

    Add HeaderTitle component
    Add timestamp props
    
    Add formatting to timestamp

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './Header.css';
+
+function HeaderTitle(props) {
+  return (
+    <Fragment>
+    <div>@LambdaSchool 
+      <li>{props.date}</li>
+    </div>
+    </Fragment>
+  );
+}
+
+export default HeaderTitle;
