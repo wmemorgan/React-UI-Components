274c608a0d2a4410970c622cc6b54fa7e3d11d8a
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-27 18:02:47 +0800

    Make entire card component a link to the React.js website

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -8,7 +8,7 @@ function CardContent() {
       <p>React makes it painless to create interactive UIs. 
         Design simple views for each state in your application.
       </p>
-      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">reactjs.org</a>
+      <p>reactjs.org</p>
     </div>
   )
 }
