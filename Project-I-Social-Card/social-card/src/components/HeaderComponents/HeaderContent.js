0310c732c43d0a8e4468969ba045bd2b81cb2d04
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-27 15:52:00 +0800

    Move HeaderTitle component inside HeaderContent component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import moment from 'moment';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+
+const currentDate = moment().format("DD MMM");
 
 function HeaderContent() {
   return (
-    <p>
-      Let's learn React by building simple interfaces with components. 
-      Don't try to overthink it, just keep it simple and have fun. 
-      Once you feel comfortable using components you are well on your way to 
-      mastering React!
+    <header className="header-content">
+      <HeaderTitle date={currentDate} />
+      <p>
+        Let's learn React by building simple interfaces with components.
+        Don't try to overthink it, just keep it simple and have fun.
+        Once you feel comfortable using components you are well on your way to
+        mastering React!
     </p>
+    </header>
   )
 }
 
