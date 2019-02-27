3294d7f256611b1194f9acf3be0f6f353c623737
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-27 15:50:08 +0800

    Enclose image inside img-wrapper

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -3,7 +3,9 @@ import './Header.css';
 
 function ImageThumbnail() {
   return (
-    <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" alt="Lambda School logo"/>
+    <div className="img-wrapper">
+      <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" alt="Lambda School logo" />
+    </div>
   )
 }
 
