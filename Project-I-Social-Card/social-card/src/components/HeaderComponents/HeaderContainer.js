c5cca38cbb00201d5177f0d8e69e2d2a1ad6378a
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-27 14:17:35 +0800

    Add HeaderContainer component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import moment from 'moment';
 import './Header.css';
+
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const currentDate = moment().format("DD MMM")
+
+function HeaderContainer() {
+  return (
+    <Fragment>
+      <ImageThumbnail />
+      <HeaderTitle date={currentDate}/>
+      <HeaderContent />
+    </Fragment>
+  );
+}
+
+export default HeaderContainer;
