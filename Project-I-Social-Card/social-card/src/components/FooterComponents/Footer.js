0c594c40feca83205c1c9eb636d44d3cc10b3bdd
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-28 23:30:08 +0800

    Convert Footer function to class object

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Footer.css';
 import LikeButton from '../LikeButtonComponents/LikeButton';
 import RefreshButton from '../RefreshButtonComponents/RefreshButton';
 
-function Footer() {
-  return (
-    <footer>
-      <nav>
-        <i className="far fa-comment"></i>
-        <RefreshButton />
-        <LikeButton />
-        <a href="mailto:contact@lambdaschool.com?subject=React Course Registration">
-          <i className="far fa-envelope"></i>
-        </a>
-      </nav>
-    </footer>
-  )
+class Footer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      likeClicks : 0,
+      refreshClicks: 0
+    }
+  }
+
+  render () {
+    return (
+      <footer>
+        <nav>
+          <i className="far fa-comment"></i>
+          <RefreshButton clicks={this.state.refreshClicks}/>
+          <LikeButton clicks={this.state.likeClicks}/>
+          <a href="mailto:contact@lambdaschool.com?subject=React Course Registration">
+            <i className="far fa-envelope"></i>
+          </a>
+        </nav>
+      </footer>
+    )
+  }
 }
 
 export default Footer;
