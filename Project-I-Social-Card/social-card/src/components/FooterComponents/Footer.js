6550fde06f7f817a6796dbffa128ad020dd4429f
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-28 17:45:52 +0800

    Deactivate LikeButton component
    Add reset `likes` counter functionality

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css';
-import LikeButton from '../LikeButtonComponents/LikeButton';
+//import LikeButton from '../LikeButtonComponents/LikeButton';
 import RefreshButton from '../RefreshButtonComponents/RefreshButton';
 
 function Footer() {
@@ -9,7 +9,7 @@ function Footer() {
       <nav>
         <i className="far fa-comment"></i>
         <RefreshButton />
-        <LikeButton />
+        {/* <LikeButton /> */}
         <a href="mailto:contact@lambdaschool.com?subject=React Course Registration">
           <i className="far fa-envelope"></i>
         </a>
--- Project-I-Social-Card/social-card/src/components/RefreshButtonComponents/RefreshButton.js
+++ Project-I-Social-Card/social-card/src/components/RefreshButtonComponents/RefreshButton.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
 import './RefreshButton.css';
+import '../LikeButtonComponents/LikeButton.css'
+
+//import LikeButton from '../LikeButtonComponents/LikeButton';
 
 class RefreshButton extends Component {
-  constructor() {
-    super()
-    this.state = { clicks: 0 }
+  constructor(props) {
+    super(props)
+    this.state = { 
+      likeClicks: 0,
+      refreshClicks: 0 
+    }
   }
 
-  incrementCounter () {
-    this.setState(prevState => ({ clicks: prevState.clicks + 1 }))
+  incrementLikes () {
+    this.setState(prevState => ({
+      likeClicks: prevState.likeClicks + 1,
+      refreshClicks: prevState.refreshClicks
+    }))
+  }
+
+  refreshLikes () {
+    this.setState(prevState => ({
+      likeClicks: 0, 
+      refreshClicks: prevState.refreshClicks + 1 
+    }))
   }
 
   render() {
     return (
-      <div className="refresh-wrapper">
-        <i className="fas fa-sync" onClick={() => this.incrementCounter()}></i>
-        <div className="refresh-count">{this.state.clicks === 0 ? '' : this.state.clicks}</div>
-      </div>
+      <>
+        <div className="refresh-wrapper">
+          <i className="fas fa-sync" onClick={() => this.refreshLikes()}></i>
+          <div className="refresh-count">{this.state.refreshClicks === 0 ? '' : this.state.refreshClicks}</div>
+        </div>
+        <div className="likes-wrapper">
+          <i className="far fa-heart" onClick={() => this.incrementLikes()}></i>
+          <div className="likes">{this.state.likeClicks === 0 ? '' : this.state.likeClicks}</div>
+        </div>
+      </>
     )
   }
 }
