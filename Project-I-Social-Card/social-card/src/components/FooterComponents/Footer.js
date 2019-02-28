ffe1dbfa367cb41cb519d6bf49fd871315a978d1
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-28 13:46:54 +0800

    Replace refresh icon with RefreshButton component

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Footer.css';
 import LikeButton from '../LikeButtonComponents/LikeButton';
+import RefreshButton from '../RefreshButtonComponents/RefreshButton';
 
 function Footer() {
   return (
     <footer>
       <nav>
         <i className="far fa-comment"></i>
-        <i className="fas fa-sync"></i>
+        <RefreshButton />
         <LikeButton />
         <a href="mailto:contact@lambdaschool.com?subject=React Course Registration">
           <i className="far fa-envelope"></i>
