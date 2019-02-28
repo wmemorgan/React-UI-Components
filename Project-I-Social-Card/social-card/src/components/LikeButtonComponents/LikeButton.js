0cbb8a659cd88345fde7ade4f0fa070a1e82f232
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-28 13:23:48 +0800

    Add counter functionality to LikeButton

--- Project-I-Social-Card/social-card/src/components/LikeButtonComponents/LikeButton.js
+++ Project-I-Social-Card/social-card/src/components/LikeButtonComponents/LikeButton.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import './LikeButton.css'
 
-
 class LikeButton extends Component {
   constructor() {
     super()
-    this.totalLikes = 0
-    this.incrementCounter = this.incrementCounter.bind(this)
+    this.state = { clicks: 0 }
   }
 
   incrementCounter() {
-    console.log('clicked!!')
-    this.totalLikes++
-    console.log(`totalLikes: ${this.totalLikes}`)
+    this.setState(prevState => ({
+      clicks: prevState.clicks + 1
+    }))
   }
 
   render () {
     return (
       <div className="likes-wrapper">
-        <i className="far fa-heart" onClick={this.incrementCounter}></i>
-        <div className="likes">{this.totalLikes}</div>
+        <i className="far fa-heart" onClick={() => this.incrementCounter()}></i>
+        <div className="likes">{this.state.clicks === 0 ? '': this.state.clicks}</div>
       </div>
     )
   }
