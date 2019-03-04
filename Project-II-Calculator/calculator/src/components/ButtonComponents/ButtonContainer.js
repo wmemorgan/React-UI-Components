dc762b96a5ddaea2965e33c44ab2eeb603e2529a
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-04 19:18:45 +0800

    Update minus sign

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -5,19 +5,14 @@ import ActionButton from './ActionButton';
 import NumberButton from './NumberButton';
 
 const numbers = [7,8,9,4,5,6,1,2,3]
-const operators = ['\xF7', 'x', '\u2212', '+', '=']
+const operators = ['\xF7', 'x', '-', '+', '=']
 
 class ButtonContainer extends Component {
   constructor(props) {
     super(props)
     this.state = { something: '' }
     console.log(this.state.something)
-    // this.showKey = this.showKey.bind(this)
   }
-  
-  // showKey(e) {
-  //   console.log(e.currentTarget.textContent)
-  // }
 
   render() {
     return (
