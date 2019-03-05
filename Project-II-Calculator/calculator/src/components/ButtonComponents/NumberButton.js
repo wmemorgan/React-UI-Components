1633444be5d57094c06e6084d99f5e0e0de99201
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-05 10:27:22 +0800

    Convert NumberButton component class back to a function

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,25 +1,13 @@
-import React, {Component } from 'react';
+import React from 'react';
 import './Button.css';
 
-class NumberButton extends Component {
-  constructor(props) {
-    super(props)
-    this.displayNumberButton = this.displayNumberButton.bind(this)
-  }
-
-  displayNumberButton(e) {
-    console.log(e.currentTarget.textContent)
-    this.props.onClick(e.currentTarget.textContent)
-  }
-
-  render () {
-    const { text, buttonStyle, showButton } = this.props
-    return (
-      <button className={buttonStyle} onClick={showButton}>
-        {text}
-      </button>
-    )
-  }
+function NumberButton(props) {
+  const { text, buttonStyle, showButton } = props
+  return (
+    <button className={buttonStyle} onClick={showButton}>
+      {text}
+    </button>
+  )
 }
 
 export default NumberButton;
