1845bfd5f2df9623117ff6b3b010c43113b9f257
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-05 10:23:46 +0800

    Convert component class back to a function

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Button.css';
 
 import ActionButton from './ActionButton';
@@ -7,41 +7,33 @@ import NumberButton from './NumberButton';
 const numbers = [7,8,9,4,5,6,1,2,3]
 const operators = ['\xF7', 'x', '-', '+', '=']
 
-class ButtonContainer extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { something: '' }
-    console.log(this.state.something)
-  }
-
-  render() {
-    return (
-      <div className="keypad">
-        <div className="data-input-keys">
-          <ActionButton buttonStyle='btn action-btn' text='clear' showButton={this.props.onDisplayButton}/>
-          {numbers.map((number) => (
-            <NumberButton
-              text={number}
-              key={number}
-              buttonStyle='btn number-btn'
-              showButton={this.props.onDisplayButton}
-            />
-          ))}
-          <ActionButton buttonStyle='btn action-btn' text='0' showButton={this.props.onDisplayButton}/>
-        </div>
-        <div className="function-keys">
-          {operators.map((operator) => (
-            <NumberButton
-              text={operator}
-              key={operator}
-              buttonStyle='btn number-btn operator-btn'
-              showButton={this.props.onDisplayButton}
-            />
-          ))}
-        </div>
+function ButtonContainer(props) {
+  return (
+    <div className="keypad">
+      <div className="data-input-keys">
+        <ActionButton buttonStyle='btn action-btn' text='clear' showButton={props.onDisplayButton} />
+        {numbers.map((number) => (
+          <NumberButton
+            text={number}
+            key={number}
+            buttonStyle='btn number-btn'
+            showButton={props.onDisplayButton}
+          />
+        ))}
+        <ActionButton buttonStyle='btn action-btn' text='0' showButton={props.onDisplayButton} />
       </div>
-    )
-  }
+      <div className="function-keys">
+        {operators.map((operator) => (
+          <NumberButton
+            text={operator}
+            key={operator}
+            buttonStyle='btn number-btn operator-btn'
+            showButton={props.onDisplayButton}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default ButtonContainer;
